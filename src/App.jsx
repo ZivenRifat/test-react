71e9c1c5fd6fbdeb37907fbe286197256b39a6db
ZivenRifat <ziven.rifatrrh@gmail.com>
2025-09-30 10:52:12 +0700

    menggunakan utility class tailwinidcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      {/* Container logo + title */}
-      <div className="flex items-center justify-center gap-6 mb-6">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="w-16 h-16" alt="Vite logo" />
-        </a>
-        <h1 className="text-4xl font-extrabold text-center">Vite + React</h1>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="w-16 h-16" alt="React logo" />
-        </a>
-      </div>
+      <div className="min-h-screen bg-neutral-800 flex flex-col items-center justify-center text-center">
+        {/* Container logo + title */}
+        <div className="flex items-center justify-center space-x-8 mb-12">
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="w-24 h-24" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="w-24 h-24 animate-spin" 
+            alt="React logo"
+            style={{ animationDuration: "0.3s" }}
+            />
+          </a>
+        </div>
 
-      {/* Subtitle */}
-      <h3 className="text-center font-semibold text-lg mb-4">Ziven Rifat</h3>
+          <h1 className="text-6xl font-extrabold text-center text-white mb-16">
+            Vite + React
+          </h1>
 
-      {/* Card */}
-      <div className="card text-center">
-        <button
-          className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-lg transition"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p className="mt-2">
-          Edit <code>src/App.jsx</code> and save to test HMR
+        {/* Subtitle */}
+        <h3 className="text-center font-semibold text-xl mb-4 mt-4 text-white">
+          Ziven Rifat
+        </h3>
+
+        {/* Card */}
+        <div className="card text-center space-y-6 rb-8">
+          <button
+            className="px-6 py-2 text-lg bg-neutral-900 hover:bg-neutral-700 text-white rounded-lg transition"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            count is {count}
+          </button>
+          <p className="text-neutral-400">
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+
+        <p className="read-the-docs mt-4 text-neutral-500 flex items-center justify-center">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-
-      <p className="read-the-docs mt-4 text-gray-600">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
