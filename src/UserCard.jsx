ccf0cb63701641c6d590ff5f564db082e9a91273
ZivenRifat <ziven.rifatrrh@gmail.com>
2025-10-23 20:07:45 +0700

    Pertemuan 6 Understanding-JSX-in-ReactJS-and-ES6

--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function UserCard(props) {
   const { name, email, street, city } = props;
+  const [clicked, setClicked] = useState (false);
+
+  // Fungsi handler
+  // function handleClick() {
+  //   setClicked(true);
+  // }
 
   return (
     <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition-shadow">
@@ -12,8 +18,14 @@ function UserCard(props) {
       <p className="text-gray-600">
         <span className="font-medium">Address:</span> {street}, {city}
       </p>
+      <button 
+      className={`${clicked ? "bg-special-green" : "bg-gray-01"} text-white p-2 rounded-md`}
+      onClick={() => setClicked(true)}
+      >
+        {clicked ? "Tombol sudah diklik" : "Silakan Klik"}
+      </button>
     </div>
   );
 }
 
-export default UserCard
+export default UserCard;
